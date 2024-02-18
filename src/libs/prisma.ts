06540fa514243feafcdb43b2f6abcad7e70a431d
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-02-18 16:50:07 -0300

    Add new files and configurations

---
+++ src/libs/prisma.ts
@@ -0,0 +1,17 @@
+// Codigo best practice para instanciar prisma en un solo lugar y reutilizar la misma instancia en toda la aplicacion
+
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
